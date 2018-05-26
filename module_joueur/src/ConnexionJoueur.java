86b34140d67a482e6c8910a3b430181695b84bcc
Valentin SAVREUX <valentin.savreux@protonmail.com>
2018-05-26 13:19:34 +0200

    Ajout design v1 pour connexion_joueur

---
+++ module_joueur/src/ConnexionJoueur.java
@@ -0,0 +1,71 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+public class ConnexionJoueur extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Accueil : Connexion");
+        primaryStage.setScene(creerConnexionJoueur());
+        primaryStage.setResizable(false);
+        primaryStage.getIcons().add(new Image("file:///C:\\Users\\bordecraft\\Desktop\\Projet Final 1A\\groupe12A\\module_joueur\\img\\logoWithoutText.png"));
+        primaryStage.show();
+    }
+
+    public Scene creerConnexionJoueur(){
+        BorderPane bp = new BorderPane();
+        bp.setLeft(creerGauche());
+        bp.setRight(creerDroite());
+        return new Scene(bp, 500,250);
+    }
+    public VBox creerGauche(){
+        VBox vBox = new VBox();
+        Image image = new Image("file:///C:\\Users\\bordecraft\\Desktop\\Projet Final 1A\\groupe12A\\module_joueur\\img\\logo.png");
+        ImageView logo = new ImageView();
+        Label slogan = new Label("La plateforme de jeux video innovante !");
+        slogan.setTextAlignment(TextAlignment.CENTER);
+        slogan.setWrapText(true);
+        slogan.setFont(Font.font("Arial", 15));
+        logo.setImage(image);
+        logo.setFitWidth(100);
+        logo.setFitHeight(100);
+        vBox.getChildren().addAll(logo, slogan);
+        vBox.setPrefWidth(250);
+        vBox.setSpacing(20);
+        vBox.setPadding(new Insets(25,0,0,25));
+        vBox.setAlignment(Pos.TOP_CENTER);
+        return vBox;
+    }
+    public VBox creerDroite(){
+        VBox vBox = new VBox();
+        Hyperlink hlRegister = new Hyperlink("Pas de compte ? S'inscrire maintenant");
+        Label title = new Label("Connexion");
+        Label lLogin = new Label("Votre nom d'utilisateur : ");
+        Label lPassword = new Label("Votre mot de passe : ");
+        TextField tfLogin = new TextField();
+        TextField tfPassword = new TextField();
+        Button btConnexion = new Button("Vers l'aventure ! -->>");
+        title.setFont(Font.font("Arial", 19));
+        vBox.setAlignment(Pos.TOP_CENTER);
+        vBox.setPadding(new Insets(18,15,0,0));
+        vBox.setPrefWidth(250);
+        vBox.setSpacing(10);
+        vBox.getChildren().addAll(title, lLogin, tfLogin, lPassword, tfPassword,hlRegister, btConnexion);
+        return vBox;
+    }
+
+}
