cf5a5984d1cd810a6804dd31bdc341d0f631d3e3
lucas.mirloup@etu.univ-orleans.fr <MirloupGB#D001F>
2018-06-01 17:40:29 +0200

    Pull manuel depuis master

---
+++ src/module_joueur/InscriptionJoueur.java
@@ -0,0 +1,40 @@
+package module_joueur;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class InscriptionJoueur extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Accueil : Inscription");
+        primaryStage.setScene(creerInscriptionJoueur());
+        primaryStage.setResizable(false);
+        File imageFile = new File("./img/logoWithoutText.png");
+        primaryStage.getIcons().add(new Image(imageFile.toURI().toString()));
+        primaryStage.show();
+    }
+
+    public Scene creerInscriptionJoueur(){
+        BorderPane bp = new BorderPane();
+        VBox vboxRegister = new VBox();
+        Label lbRegister = new Label("Inscription");
+        Label lbPseudo = new Label("Votre pseudo");
+        Label lPassword = new Label("Votre mot de passe");
+        Label lMail = new Label("Votre mail");
+        
+
+        return new Scene(bp, 500,300);
+    }
+}
---
+++ src/module_puissance4/Colonne.java
@@ -0,0 +1,32 @@
+package sample;
+
+
+import java.util.ArrayList;
+
+public class Colonne extends ArrayList<Integer> { // Modèle d'une colonne du plateau
+    // Une case est modélisée en contenant un entier comme ceci :
+            // 0 : Pas de pion
+            // 1 ou 2 : Pion du joueur 1 ou 2
+
+    public Colonne(){
+        super();
+        for (int l=0;l<6;l++){
+            this.add(0);
+        }
+    }
+
+    public int addPion(int numJ){ // renvoie le num de la ligne où le pion a atterrit
+        int l = 0;
+        while (this.get(l)!=0){l++;}
+        this.set(l,numJ);
+        return l;
+    }
+
+    public boolean isNotFull(){
+        return this.get(5)==0;
+    }
+    public boolean isFull(){
+        return this.get(5)!=0;
+    }
+    // si la case la plus haute est occupée, la colonne est pleine
+}
