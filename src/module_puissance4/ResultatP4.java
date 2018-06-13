a7dd9ca6559755dff3ab85592e60a20bedea07a7
MERILLON Mathieu <mathieumeri@hotmail.fr>
2018-06-13 18:55:36 +0200

    AJout Page de Choix du joueur au puissance 4 : tests OK

---
+++ src/module_puissance4/ResultatP4.java
@@ -0,0 +1,12 @@
+package module_puissance4;
+
+public class ResultatP4{
+
+    public boolean contientPuissance;
+    public int nbPionsAlignes;
+
+    public ResultatP4(boolean res,int n){
+        this.contientPuissance = res;
+        this.nbPionsAlignes = n;
+    }
+}
