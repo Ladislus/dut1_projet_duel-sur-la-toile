f722306fd421d6248472d89122578c22401077c9
Lucas Mirloup <lucas.mirloup@etu.univ-orleans.fr>
2018-06-14 10:44:32 +0200

    ActionBouton

--- src/module_joueur/ActionBouton.java
+++ src/module_joueur/ActionBouton.java
@@ -7,30 +7,25 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 class ActionBouton implements EventHandler<ActionEvent> {
+    private Stage primaryStage;
 
-  private Stage primaryStage;
+    public ActionBouton(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
 
-  public ActionBouton(Stage primaryStage) { this.primaryStage = primaryStage; }
-
-  public void handle(ActionEvent actionEvent) {
-
-    Button bt = (Button) actionEvent.getTarget();
-
-    if (bt.getText().contains("Retour")) {
-
-      ConnexionJoueur connection = new ConnexionJoueur(this.primaryStage);
-
-      primaryStage.setTitle(connection.getTitle());
-      primaryStage.setScene(new Scene(connection, VariablesJoueur.DEFAULT_CONNECTION_WIDTH, VariablesJoueur.DEFAULT_CONNECTION_HEIGHT)); }
-
-    else if (bt.getText().contains("Vers l'aventure !")) {
-
-      ConnexionJoueur connexion = (ConnexionJoueur) this.primaryStage.getScene().getRoot();
-
-      String login = connexion.getTfLogin().getText();
-      String password = connexion.getTfPassword().getText();
-
-      if (login.size() && password.size()){}
-
-      //TODO avec le dashboard
-    }}}
+    public void handle(ActionEvent actionEvent) {
+        Button bt = (Button) actionEvent.getTarget();
+        if (bt.getText().contains("Retour")) {
+            ConnexionJoueur connection = new ConnexionJoueur(this.primaryStage);
+            primaryStage.setTitle(connection.getTitle());
+            primaryStage.setScene(new Scene(connection, VariablesJoueur.DEFAULT_CONNECTION_WIDTH, VariablesJoueur.DEFAULT_CONNECTION_HEIGHT)); }
+        else if (bt.getText().contains("Vers l'aventure !")) {
+            ConnexionJoueur connexion = (ConnexionJoueur) this.primaryStage.getScene().getRoot();
+            String login = connexion.getTfLogin().getText();
+            String password = connexion.getTfPassword().getText();
+            /*if (login.size() && password.size()){
+            //TODO avec le dashboard
+            }*/
+        }
+    }
+}
