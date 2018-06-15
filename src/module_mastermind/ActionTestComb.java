8ec7d528f095522d1d4e78981124e6e2622816a2
Benjamin <benjamin.saint-mars@etu.univ-orleans.fr>
2018-06-15 10:06:05 +0200

    Implémentation de ActionTestComb.java => pas testé

--- src/module_mastermind/ActionTestComb.java
+++ src/module_mastermind/ActionTestComb.java
@@ -2,14 +2,39 @@ package module_mastermind;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
 
 public class ActionTestComb implements EventHandler<ActionEvent> {
 
+    private PartieM mastermind;
 
-
-    public ActionTestComb(PartieM partieM) {
+    public ActionTestComb(PartieM mastermind) {
+        this.mastermind = mastermind;
     }
 
     @Override
-    public void handle(ActionEvent actionEvent){}
+    /***
+     *
+     */
+    public void handle(ActionEvent actionEvent){
+        PlateauM plateauM = this.mastermind.getPlateau();
+        plateauM.jouerUnCoup();
+        if (plateauM.aTrouveCombi()){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("VICTOIRE !");
+            alert.setHeaderText("Vous avez gagné !");
+            alert.setContentText("Bravo, vous avez trouvé la combinaison !\n" +
+                    " Votre score est de xxx points ! \n" +
+                    "Cliquer sur le bouton \"Rejouer\" pour refaire une partie, et sur \"Quitter\" pour arrêter.\n" +
+                    "Bon jeu !");
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("DÉFAITE ...");
+            alert.setHeaderText("Vous avez perdu ...");
+            alert.setContentText("Malheureusement, vous n'avez pas réussi cette fois-ci.\n" +
+                    "Retentez votre chance en cliquant sur \"Rejouer\" ou arrêtez vous ici en cliquant sur \"Quitter\".");
+        }
+        this.mastermind.majAffichage();
+    }
 }
