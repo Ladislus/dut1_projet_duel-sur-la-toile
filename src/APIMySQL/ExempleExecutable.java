af13b52179ce335bb8180d72a7038a6c27b87628
Lucas Mirloup <lucas.mirloup@etu.univ-orleans.fr>
2018-06-15 14:11:34 +0200

    Fix

--- src/APIMySQL/ExempleExecutable.java
+++ src/APIMySQL/ExempleExecutable.java
@@ -8,20 +8,11 @@ public class ExempleExecutable {
 
         ConnexionMySQL co = new ConnexionMySQL("192.168.1.100", "serveurDeJeux", "dst", "dst");
 
-<<<<<<< HEAD
-        GestionBD.updateStatement(co,"INSERT INTO ROLE VALUES ('ADMIN')");
-
-        try {
-            Utilisateur.creerUtilisateur(co,"test","test@gmail.com","couscous","admin");
-        } catch (UtilisateurException e) {
-            e.printStackTrace();
-=======
         try {
             GestionBD.updateStatement(co,"INSERT INTO ROLE VALUES ('ADMIN')");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             //e.printStackTrace();
->>>>>>> e0d98be32e79b887b8687659bb663004dd9fdfa0
         }
 
         try {
