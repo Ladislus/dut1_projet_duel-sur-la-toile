cda16d210eaf2081810923ca330de454a7611009
Nolan Martins <nolan.martins@etu.univ-orleans.fr>
2018-06-15 15:50:11 +0200

    modification fichier VoirStatistiques pour que le code soit plus lisible

--- src/module_administrateur/VoirStatistiques.java
+++ src/module_administrateur/VoirStatistiques.java
@@ -36,76 +36,217 @@ public class VoirStatistiques extends BorderPane {
         this.setTop(haut);
     }
 
+
+    public DatePicker creerDatePicker(){
+      DatePicker dp = new DatePicker();
+      dp.setPrefWidth(120);
+
+      return dp;
+    }
+
+
+    public Label creerLabelDatePicker(){
+      Label lau = new Label("au");
+
+      return lau;
+    }
+
+
+    public HBox creerHBoxDatePicker(){
+      HBox hbdate = new HBox(15);
+      hbdate.getChildren().addAll(creerDatePicker(), creerLabelDatePicker(), creerDatePicker());
+
+      return hbdate;
+    }
+
+
+    public Label creerLabelJoueursConnectes(){
+      Label jconnecte = new Label("Joueurs connectés : ");
+
+      return jconnecte;
+    }
+
+
+    public Label creerLabelNbJoueursInscrits(){
+      Label nbjinscrits = new Label("Nombre de joueurs inscrits : ");
+
+      return nbjinscrits;
+    }
+
+
+    public Label creerLabelNbPartiesPlateforme(){
+      Label nbpartiestotales = new Label("Nombre de parties totales de la plateforme : ");
+
+      return nbpartiestotales;
+    }
+
+
+    public Label creerLabelStatsJeu(){
+      Label statsjeux = new Label("Statistiques des jeux");
+      statsjeux.setStyle("-fx-font-weight: bold");
+
+      return statsjeux;
+    }
+
+
+    public ComboBox creerComboBoxOptions(){
+      ObservableList<String> optionsJeux = FXCollections.observableArrayList("Puissance 4", "Mastermind");
+      ComboBox cbJeux = new ComboBox(optionsJeux);
+      cbJeux.setPrefWidth(300);
+
+      return cbJeux;
+    }
+
+
+    public Label creerLabelNbPartiesJeu(){
+      Label nbpartiestotalesjeu = new Label("Nombre de parties totales : ");
+
+      return nbpartiestotalesjeu;
+    }
+
+
+    public Label creerLabelMeilleurJoueur(){
+      Label meilleurj = new Label("Meilleur joueur : ");
+
+      return meilleurj;
+    }
+
+
+    public Label creerLabelStatsPeriode(){
+      Label statsperiode = new Label("Statistiques sur une période : ");
+      statsperiode.setStyle("-fx-font-weight: bold");
+
+      return statsperiode;
+    }
+
+
+    public Label creerLabelNbPartiesJeuPeriode(){
+      Label nbpartiestotalesjeuperiode = new Label("Nombre de parties totales : ");
+
+      return nbpartiestotalesjeuperiode;
+    }
+
+
+    public Label creerLabelMeilleurJoueurPeriode(){
+      Label meilleurjperiode = new Label("Meilleur joueur : ");
+
+      return meilleurjperiode;
+    }
+
+
+    public VBox creerVBoxStatsJeux(){
+      VBox vbstatsjeux = new VBox(10);
+      vbstatsjeux.getChildren().addAll(creerComboBoxOptions(), creerLabelNbPartiesPlateforme(),
+        creerLabelMeilleurJoueur(), creerLabelStatsPeriode(), creerHBoxDatePicker(),
+          creerLabelNbPartiesJeuPeriode(), creerLabelMeilleurJoueurPeriode());
+      vbstatsjeux.setPadding(new Insets(5,10,5,10));
+      vbstatsjeux.setStyle("-fx-border-color: black;");
+      vbstatsjeux.setPrefHeight(250);
+
+      return vbstatsjeux;
+    }
+
+
     public void gauche() {
         VBox vbgauche = new VBox(10);
-        VBox vbstatsjeux = new VBox(10);
-        HBox hbdate = new HBox(15);
-
-        Label jconnecte = new Label("Joueurs connectés : ");
-        Label nbjinscrits = new Label("Nombre de joueurs inscrits : ");
-        Label nbpartiestotales = new Label("Nombre de parties totales de la plateforme : ");
-        Label statsjeux = new Label("Statistiques des jeux");
-        statsjeux.setStyle("-fx-font-weight: bold");
-        ObservableList<String> optionsJeux = FXCollections.observableArrayList("Puissance 4", "Mastermind");
-        ComboBox cbJeux = new ComboBox(optionsJeux);
-        Label nbpartiestotalesjeu = new Label("Nombre de parties totales : ");
-        Label meilleurj = new Label("Meilleur joueur : ");
-        Label statsperiode = new Label("Statistiques sur une période : ");
-        statsperiode.setStyle("-fx-font-weight: bold");
-        Label nbpartiestotalesjeuperiode = new Label("Nombre de parties totales : ");
-        Label meilleurjperiode = new Label("Meilleur joueur : ");
-        DatePicker dp1 = new DatePicker();
-        dp1.setPrefWidth(120);
-        Label lau = new Label("au");
-        DatePicker dp2 = new DatePicker();
-        dp2.setPrefWidth(120);
-
-        hbdate.getChildren().addAll(dp1, lau, dp2);
-
-        vbstatsjeux.getChildren().addAll(cbJeux, nbpartiestotalesjeu, meilleurj, statsperiode,
-          hbdate, nbpartiestotalesjeuperiode, meilleurjperiode);
-        vbstatsjeux.setPadding(new Insets(5,10,5,10));
-        vbstatsjeux.setStyle("-fx-border-color: black;");
-        vbstatsjeux.setPrefHeight(250);
-
-
-        vbgauche.getChildren().addAll(jconnecte, nbjinscrits, nbpartiestotales, statsjeux,
-          vbstatsjeux);
-
+        vbgauche.getChildren().addAll(creerLabelJoueursConnectes(), creerLabelNbJoueursInscrits(),
+          creerLabelNbPartiesPlateforme(), creerLabelStatsJeu(), creerVBoxStatsJeux());
         vbgauche.setPadding(new Insets(20,25,20,25));
+
         this.setLeft(vbgauche);
     }
 
-    public void droite() {
-      VBox vbdroite = new VBox(10);
-      VBox vbmeilleurjplateforme = new VBox(10);
-      HBox hbj1 = new HBox(120);
-      HBox hbj2 = new HBox(120);
 
+    public Label creerLabelNbPartiesEnCours(){
       Label nbpartiesencours = new Label("Nombre de parties en cours : ");
+
+      return nbpartiesencours;
+    }
+
+
+    public Label creerLabelRecordConnectes(){
       Label recordconnectes = new Label("Record de connectés : ");
+
+      return recordconnectes;
+    }
+
+
+    public Label creerLabelMeilleursJoueurs(){
       Label meilleurjplateforme = new Label("Meilleurs joueurs de la plateforme");
+
+      return meilleurjplateforme;
+    }
+
+
+    public Label creerLabelVide(){
       Label vide = new Label(" ");
-      Label vide2 = new Label(" ");
+
+      return vide;
+    }
+
+
+    public Label creerLabelJoueur1(){
       Label lj1 = new Label("Joueur1");
+
+      return lj1;
+    }
+
+
+    public Label creerLabelJoueur2(){
       Label lj2 = new Label("Joueur2");
+
+      return lj2;
+    }
+
+
+    public Label creerLabelRatio1(){
       Label ratioj1 = new Label("75% V/D");
+
+      return ratioj1;
+    }
+
+
+    public Label creerLabelRatio2(){
       Label ratioj2 = new Label("65% V/D");
 
+      return ratioj2;
+    }
 
-      hbj1.getChildren().addAll(lj1, ratioj1);
-      hbj2.getChildren().addAll(lj2, ratioj2);
 
-      vbmeilleurjplateforme.getChildren().addAll(meilleurjplateforme, hbj1, hbj2);
+    public HBox creerHBoxJ1(){
+      HBox hbj1 = new HBox(120);
+      hbj1.getChildren().addAll(creerLabelJoueur1(), creerLabelRatio1());
+
+      return hbj1;
+    }
+
+
+    public HBox creerHBoxJ2(){
+      HBox hbj2 = new HBox(120);
+      hbj2.getChildren().addAll(creerLabelJoueur2(), creerLabelRatio2());
+
+      return hbj2;
+    }
+
+
+    public VBox creerVBoxMeilleursJoueurs(){
+      VBox vbmeilleurjplateforme = new VBox(10);
+      vbmeilleurjplateforme.getChildren().addAll(creerLabelMeilleursJoueurs(), creerHBoxJ1(),
+        creerHBoxJ2());
       vbmeilleurjplateforme.setPadding(new Insets(5,10,5,10));
       vbmeilleurjplateforme.setStyle("-fx-border-color: black;");
       vbmeilleurjplateforme.setPrefHeight(250);
 
-      vbdroite.getChildren().addAll(nbpartiesencours, recordconnectes, vide, vide2,
-        vbmeilleurjplateforme);
+      return vbmeilleurjplateforme;
+    }
 
+
+    public void droite() {
+      VBox vbdroite = new VBox(10);
+      vbdroite.getChildren().addAll(creerLabelNbPartiesEnCours(), creerLabelRecordConnectes(),
+        creerLabelVide(), creerLabelVide(), creerVBoxMeilleursJoueurs());
       vbdroite.setPadding(new Insets(20,25,20,25));
-      this.setRight(vbdroite);
 
+      this.setRight(vbdroite);
     }
 }
