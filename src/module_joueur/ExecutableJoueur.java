ab34213dd147c5c5c930b99fbe265c6b0779d003
Valentin Savreux <valentin.savreux@protonmail.com>
2018-06-15 14:41:10 +0200

    fix

--- src/module_joueur/ExecutableJoueur.java
+++ src/module_joueur/ExecutableJoueur.java
@@ -2,25 +2,27 @@ package module_joueur;
 
 import APIMySQL.GestionBD;
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 
 public class ExecutableJoueur extends Application {
 
   public static void main(String [] args) { launch(args); }
 
-
   @Override
   public void start(Stage primaryStage) {
-      GestionBD laConnection = null;
-      EditionProfil editionProfil = new EditionProfil(primaryStage);
-      ConnexionJoueur cj = new ConnexionJoueur(primaryStage, laConnection);
 
-      primaryStage.setTitle(editionProfil.getTitle());
-      primaryStage.setScene(new Scene(editionProfil, VariablesJoueur.DEFAULT_EDITERPROFILE_WIDTH, VariablesJoueur.DEFAULT_EDITERPROFILE_HEIGHT));
-      primaryStage.setResizable(VariablesJoueur.IS_RESIZABLE);
-      primaryStage.getIcons().add(VariablesJoueur.LOGO);
-      primaryStage.show();
-  }
-}
+    GestionBD laConnection = null;
+
+    try { laConnection = new GestionBD("192.168.1.100", "serveurDeJeux", "dst", "dst"); }
+    catch (ClassNotFoundException e) {} //TODO faire l'alerte
+
+    ConnexionJoueur connection = new ConnexionJoueur(primaryStage, laConnection);
+
+    primaryStage.setTitle(connection.getTitle());
+    primaryStage.setScene(new Scene(connection, VariablesJoueur.DEFAULT_CONNECTION_WIDTH, VariablesJoueur.DEFAULT_CONNECTION_HEIGHT));
+
+    primaryStage.setResizable(VariablesJoueur.IS_RESIZABLE);
+    primaryStage.getIcons().add(VariablesJoueur.LOGO);
+    primaryStage.show(); }}
