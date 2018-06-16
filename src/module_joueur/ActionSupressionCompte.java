37b7f1cf55939dfb850b5b6da2182d11f88262db
Valentin Savreux <valentin.savreux@protonmail.com>
2018-06-17 00:30:23 +0200

    Ajout Action de suppression d'un compte

--- src/module_joueur/ActionSupressionCompte.java
+++ src/module_joueur/ActionSupressionCompte.java
@@ -1,6 +1,8 @@
 package module_joueur;
 
+
 import APIMySQL.ConnexionMySQL;
+import APIMySQL.GestionBD;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -9,12 +11,12 @@ import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
-public class ActionDeconnexion implements EventHandler<ActionEvent> {
+public class ActionSupressionCompte implements EventHandler<ActionEvent> {
 
     Stage primaryStage;
     ConnexionMySQL laConnection;
 
-    public ActionDeconnexion(Stage primaryStage, ConnexionMySQL laConnection){
+    public ActionSupressionCompte(Stage primaryStage, ConnexionMySQL laConnection){
         this.laConnection = laConnection;
         this.primaryStage = primaryStage;
     }
@@ -23,14 +25,15 @@ public class ActionDeconnexion implements EventHandler<ActionEvent> {
     public void handle(ActionEvent actionEvent) {
         ConnexionJoueur connexion = new ConnexionJoueur(this.primaryStage, this.laConnection);
 
-        Alert alert = new Alert(Alert.AlertType.WARNING);
+        Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Attention");
-        alert.setContentText("Voulez-vous vraiment vous deconnecter ?");
+        alert.setContentText("Etes vous sure de vouloir supprimer votre compte ?");
         ButtonType okButton = new ButtonType("Oui", ButtonBar.ButtonData.YES);
         ButtonType noButton = new ButtonType("Non", ButtonBar.ButtonData.NO);
         alert.getButtonTypes().setAll(okButton, noButton);
         alert.showAndWait().ifPresent(type -> {
             if (type.getButtonData().toString().equals("YES")) {
+                //todo: change activeUt into the BD
                 this.primaryStage.setTitle(connexion.getTitle());
                 this.primaryStage.setScene(new Scene(connexion, VariablesJoueur.DEFAULT_CONNECTION_WIDTH, VariablesJoueur.DEFAULT_CONNECTION_HEIGHT));
             }
