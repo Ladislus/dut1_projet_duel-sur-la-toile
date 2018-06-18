a41c7b6158d292570b0c01a07fc6a539c3fb97eb
Lucas Mirloup <lucas.mirloup@etu.univ-orleans.fr>
2018-06-18 10:01:24 +0200

    Passage de la Connexion en static

--- src/APIMySQL/ExempleExecutable.java
+++ src/APIMySQL/ExempleExecutable.java
@@ -5,29 +5,28 @@ import java.sql.SQLException;
 public class ExempleExecutable {
     public static void main(String[] args) throws ClassNotFoundException {
         // TEST : Essai de passer en bibliothèques statiques.
-
-        ConnexionMySQL co = new ConnexionMySQL("localhost", "serveurDeJeux", "dst", "dst");
-
         try {
-            GestionBD.updateStatement(co,"INSERT INTO ROLE VALUES ('ADMIN')");
+            GestionBD.updateStatement("INSERT INTO ROLE VALUES ('ADMIN')");
         } catch (SQLException e) {
             //e.printStackTrace();
         }
 
         try {
-            Utilisateur.creerUtilisateur(co,"test","test@gmail.com","M","couscous","admin");
+            Utilisateur.creerUtilisateur("test","test@gmail.com","M","couscous","admin");
         } catch (UtilisateurException e) {
             //e.printMessage();
         }
 
         try {
-            System.out.println(Utilisateur.isMdpValide(co,"test","couscous"));
-            System.out.println(Utilisateur.isMdpValide(co,"test","test"));
-            System.out.println(Utilisateur.isActivated(co,"test"));
-            System.out.println(Utilisateur.isMdpValide(co,"test2","test"));
+            System.out.println(Utilisateur.isMdpValide("test","couscous"));
+            System.out.println(Utilisateur.isMdpValide("test","test"));
+            System.out.println(Utilisateur.isActivated("test"));
+            System.out.println(Utilisateur.isMdpValide("test2","test"));
         } catch (UtilisateurException e) {
             //e.printMessage();
         }
+
+        //Partie.creerPartie();
 
     }
 }
--- src/APIMySQL/Partie.java
+++ src/APIMySQL/Partie.java
@@ -7,11 +7,11 @@ import java.util.Collections;
 public class Partie {
     private Partie(){}
 
-    public void creerPartie(ConnexionMySQL co, int idJeu, int idUt1, int idUt2){
+    public static void creerPartie(int idJeu, int idUt1, int idUt2){
         ArrayList<Object> donnees = new ArrayList<>();
         Collections.addAll(donnees,(int)(System.currentTimeMillis()/1000L),0,"",idJeu,idUt1,0,idUt2,0);
         try {
-            GestionBD.updatePreparedStatement(co,"INSERT INTO PARTIE (debutPa,numEtape,etatPartie,idJeu,idUt1,score1,idUt2,score2) VALUES (?,?,?,?,?,?,?,?)",donnees);
+            GestionBD.updatePreparedStatement("INSERT INTO PARTIE (debutPa,numEtape,etatPartie,idJeu,idUt1,score1,idUt2,score2) VALUES (?,?,?,?,?,?,?,?)",donnees);
         } catch (SQLException e) {
             e.printStackTrace();
         }
