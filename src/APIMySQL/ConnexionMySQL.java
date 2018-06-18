8ad3deb23ae4df417649f533d7a90c1e22dcbaa2
Lucas Mirloup <lucas.mirloup@etu.univ-orleans.fr>
2018-06-18 11:54:21 +0200

    Passage partiel en static de ConnexionMySQL, création de jeu maj

--- src/APIMySQL/ConnexionMySQL.java
+++ src/APIMySQL/ConnexionMySQL.java
@@ -3,33 +3,23 @@ package APIMySQL;
 import java.sql.*;
 
 public class ConnexionMySQL {
-	private Connection mysql;
-	private boolean connecte = false;
+	public static Connection mysql;
+
 	public ConnexionMySQL(String IP, String database, String user, String mdp) throws ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver");
 		try {
-			this.mysql = DriverManager.getConnection("jdbc:mysql://"+ IP +":3306/"+ database, user, mdp);
-			this.connecte=true;
+			mysql = DriverManager.getConnection("jdbc:mysql://"+ IP +":3306/"+ database, user, mdp);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void close() throws SQLException {
-		this.mysql.close();
-		this.connecte=false;
-	}
-
-	public boolean isConnecte() { return this.connecte;}
-	public Blob createBlob()throws SQLException{
-		return this.mysql.createBlob();
-	}
 	public Statement createStatement() throws SQLException {
-		return this.mysql.createStatement();
+		return mysql.createStatement();
 	}
 
 	public PreparedStatement prepareStatement(String requete) throws SQLException{
-		return this.mysql.prepareStatement(requete);
+		return mysql.prepareStatement(requete);
 	}
 
 }
--- src/APIMySQL/Jeu.java
+++ src/APIMySQL/Jeu.java
@@ -1,9 +1,16 @@
 package APIMySQL;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Jeu {
     private Jeu(){}
 
-    public static void creerJeu(){
-        //GestionBD.
+    public static void creerJeu(String nomJeu, String regleJeu, byte[] jarJeu, int idTy, byte[] image) throws SQLException {
+        ArrayList<Object> donnees = new ArrayList<>();
+        //ConnexionMySQL.mysql.createBlob(jarJeu)
+        Collections.addAll(donnees,nomJeu,regleJeu,jarJeu,1,idTy,image);
+        GestionBD.updatePreparedStatement("INSERT INTO JEU (nomJeu,regleJeu,jarJeu,activeJeu,idTy,image) VALUES (?,?,?,?,?,?)",donnees);
     }
 }
