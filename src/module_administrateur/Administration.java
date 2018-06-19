0456144c41dbfd2c48fc4876427b93fe058df383
Nolan Martins <nolan.martins@etu.univ-orleans.fr>
2018-06-19 17:20:24 +0200

    modifs non finies affichage image sur la page d'accueil

--- src/module_administrateur/Administration.java
+++ src/module_administrateur/Administration.java
@@ -8,13 +8,9 @@ public class Administration {
 
     private ArrayList<Joueur> joueurAactiver;
 
-    private ArrayList<Rapport> listeRapport;
-    private ArrayList<Rapport> listeRapportLu;
 
     public Administration() {
         this.joueurAactiver = new ArrayList<>();
-        this.listeRapport = new ArrayList<>();
-        this.listeRapportLu = new ArrayList<>();
     }
 
     //GERER JOUEUR
@@ -45,28 +41,4 @@ public class Administration {
         return cpt;
     }
 
-    //GERER RAPPORT
-    public void ajouterRapport(Rapport r) {
-        this.listeRapport.add(r);
-    }
-
-    public ArrayList<Rapport> getRapport() {
-        return this.listeRapport;
-    }
-
-    public void ajouterRapportLu(Rapport r) {
-        this.listeRapportLu.add(r);
-    }
-
-    public void retirerRapportLu(Rapport r) {
-        this.listeRapportLu.remove(r);
-    }
-
-    public ArrayList<Rapport> getRapportLu() {
-        return this.listeRapportLu;
-    }
-
-    public void supprimerRapportLu(Rapport r) {
-        this.listeRapport.remove(r);
-    }
 }
