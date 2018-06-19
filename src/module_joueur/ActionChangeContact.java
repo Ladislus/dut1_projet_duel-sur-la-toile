0d04176009a3b6baad00fa3becfbb54ccc787f3f
MERILLON Mathieu <mathieumeri@hotmail.fr>
2018-06-19 18:57:19 +0200

    Messagerie en avancement

---
+++ src/module_joueur/ActionChangeContact.java
@@ -0,0 +1,29 @@
+package module_joueur;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
+
+/**
+ * Controleur du changement de contact dans la messagerie
+ */
+public class ActionChangeContact implements EventHandler<ActionEvent> {
+
+    private final Messagerie messagerie;
+
+    public ActionChangeContact(Messagerie messagerie) {
+        this.messagerie = messagerie;
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        Button source = (Button) actionEvent.getSource();
+        String nouvContact = source.getText();
+        if (nouvContact == "ACCUEIL"){
+            nouvContact = "ACC";
+        }
+        this.messagerie.setNomContactCour(nouvContact);
+        this.messagerie.majMessages();
+    }
+}
