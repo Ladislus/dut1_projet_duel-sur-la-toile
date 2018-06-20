35328a1bd01b801eeda223d3d456a209961ef8ba
Leo Rouillard <leo.rouillard@etu.univ-orleans.fr>
2018-06-20 08:47:55 +0200

    refonte vue gérer joueur + ajout attribut joueur + finition de activation/desactivation joueur

--- src/module_administrateur/Administration.java
+++ src/module_administrateur/Administration.java
@@ -3,42 +3,59 @@ package module_administrateur;
 import APIMySQL.*;
 import java.util.ArrayList;
 import java.sql.*;
+import java.util.*;
 
+/** Modèle de l'administration */
 public class Administration {
 
-    private ArrayList<Joueur> joueurAactiver;
+    private ArrayList<Joueur> listeJmodifStatut;
 
-
+    /** Constructeur de l'administration avec les initialisations */
     public Administration() {
-        this.joueurAactiver = new ArrayList<>();
+        this.listeJmodifStatut = new ArrayList<>();
     }
 
-    //GERER JOUEUR
-    public void ajouterListeActiver(Joueur joueur) {
-        this.joueurAactiver.add(joueur);
+    /** Réinitialise tous les éléments à vide */
+    public void majAdmin() {
+        this.listeJmodifStatut = new ArrayList<>();
     }
 
-    public void retirerListeActiver(Joueur joueur) {
-        this.joueurAactiver.remove(joueur);
+    ////////////////////
+    // GERER JOUEUR //
+    /////////////////
+
+    /** Ajoute à la liste les joueurs à changer de statut (activé/désactivé) */
+    public void ajouterListeModif(Joueur joueur) {
+        this.listeJmodifStatut.add(joueur);
     }
 
-    public void retirerTousJoueurActiver(ArrayList<Joueur> liste) {
-        ArrayList<Joueur> newListe = new ArrayList<>();
-        newListe.addAll(liste);
-        for (Joueur j : liste) {
-            newListe.remove(j);
-        }
-        this.joueurAactiver = newListe;
+    /** Retire de la liste les joueurs à changer de statut (activé/désactivé) */
+    public void retirerListeModif(Joueur joueur) {
+        this.listeJmodifStatut.remove(joueur);
     }
 
-    public ArrayList<Joueur> getJoueurAactiver() {
-        return this.joueurAactiver;
+    /** Retourne la liste des joueurs à changer de statut (activé/désactivé) */
+    public ArrayList<Joueur> getListeModif() {
+        return this.listeJmodifStatut;
     }
 
-    public int cptJoueurActiver() {
-        int cpt = 0;
-        cpt = GestionBD.selectPreparedStatement("select * from UTILISATEUR where activeUt IS NOT TRUE;").get("pseudoUt").size();
-        return cpt;
+    ///////////////////
+    // REQUETTE SQL //
+    /////////////////
+
+    /** Retourne sous la forme d'un dictionnaire avec leur informations tous les utilisateurs */
+    public HashMap<String, List<Object>> requetteListeJoueur() {
+        return GestionBD.selectPreparedStatement("select * from UTILISATEUR;");
+    }
+
+    /** Permet de désactiver un joueur donné en paramètre */
+    public void desactiverJoueur(Joueur j) {
+        Utilisateur.setUserInfo("activeUt", 0, "pseudoUt", j.getPseudo());
+    }
+
+    /** Permet d'activer un joueur donné en paramètre */
+    public void activerJoueur(Joueur j) {
+        Utilisateur.setUserInfo("activeUt", 1, "pseudoUt", j.getPseudo());
     }
 
 }
